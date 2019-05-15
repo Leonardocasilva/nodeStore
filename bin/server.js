08ce5ccf9f62eaace072e43488be1ae49653a207
Leonardo Silva <lcsilva80@yahoo.com.br>
2019-05-14 23:14:05 -0300

    Iniciando o projeto

---
+++ bin/server.js
@@ -0,0 +1,61 @@
+'use strict'
+const app = require('../src/app')
+const http = require('http');
+const debug = require('debug')('stormclouds:server');
+
+const port = normalizePort(process.env.port || 3000);
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+console.log('API is running on port ' + port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port
+    }
+
+    return false;
+}
+
+function onError (erro) {
+    if (erro.syscall !== 'listen') {
+        throw erro;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.log(bind + ' required elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.log(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening () {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe' + addr
+        : 'port' + addr;
+
+    debug('Listining on' + bind);
+}
