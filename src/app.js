5e9e8d6d55cbc273473070261a675668c42bf2b3
Leonardo Silva <lcsilva80@yahoo.com.br>
2019-05-14 23:35:02 -0300

    route adjust

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const router = express.Router();
 
 // Carregar as rotas
 const indexRoute = require('./routes/index-route');
@@ -12,8 +11,6 @@ const productRoute = require('./routes/product-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
